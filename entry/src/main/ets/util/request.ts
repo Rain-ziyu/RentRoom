ba7c978d9729fca787c696d1cbf4ac2765d81200
ziyu <1874300301@qq.com>
2025-02-18 20:19:45 +0800

    feat(HTTP): 请求util
    请求工具封装

---
+++ entry/src/main/ets/util/request.ts
@@ -0,0 +1,42 @@
+import axios, { AxiosError, AxiosResponse, InternalAxiosRequestConfig } from '@ohos/axios';
+import promptAction from '@ohos.promptAction';
+import AnyObject from '../models/HttpModel'
+const request = axios.create({
+  baseURL: ''
+})
+// 封装请求时的统一配置
+request.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    return config
+  }
+)
+// 相应内容判断
+request.interceptors.response.use(
+  (response: AxiosResponse) => {
+    if (response.data.code === 200) {
+      return response.data.data;
+    } else {
+      return Promise.reject(response.data.message);
+    }
+  },
+  (error: AxiosError) => {
+    promptAction.showToast({ message: error.message })
+    return Promise.reject(error.message)
+  }
+)
+
+
+export default class Http{
+  get<T>(url:string,params?:AnyObject){
+    return request.get<any,T>(url,{params})
+  }
+  post<T>(url:string,data?:AnyObject){
+    return request.post<any,T>(url,{data})
+  }
+  put<T>(url:string,data?:AnyObject){
+    return request.put<any,T>(url,{data})
+  }
+  delete<T>(url:string,params?:AnyObject){
+    return request.delete<any,T>(url,{params})
+  }
+}
